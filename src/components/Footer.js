164728c7bfba674c27327dbca64c57d86756ac91
Chadi Ayari <98648139+ayarichadi@users.noreply.github.com>
2022-04-04 13:58:58 +0530

    Updated header & footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,7 +16,7 @@ function Footer() {
           <h3>Designed and Developed by Chadi Ayari</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © year SB</h3>
+          <h3>© 2022 Chadi Ayari. All rights somewhat reserved.</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,8 @@ import Container from "react-bootstrap/Container";
 import logo from "../Assets/logo.png";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
-import { CgGitFork } from "react-icons/cg";
 import { ImBlog } from "react-icons/im";
 import {
-  AiFillStar,
   AiOutlineHome,
   AiOutlineFundProjectionScreen,
   AiOutlineUser,
@@ -108,8 +106,7 @@ function NavBar() {
                 target="_blank"
                 className="fork-btn-inner"
               >
-                <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
-                <AiFillStar style={{ fontSize: "1.1em" }} />
+                {"Contact me"}
               </Button>
             </Nav.Item>
           </Nav>
