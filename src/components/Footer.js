2e1128474949afa32202f2219d3210b276fcca92
Chadi Ayari <98648139+ayarichadi@users.noreply.github.com>
2022-04-04 12:18:14 +0530

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import {
+  AiFillGithub,
+  AiFillFacebook,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Footer() {
+  let date = new Date();
+  return (
+    <Container fluid className="footer">
+      <Row>
+        <Col md="4" className="footer-copywright">
+          <h3>Designed and Developed by Chadi Ayari</h3>
+        </Col>
+        <Col md="4" className="footer-copywright">
+          <h3>Copyright © year SB</h3>
+        </Col>
+        <Col md="4" className="footer-body">
+          <ul className="footer-icons">
+            <li className="social-icons">
+              <a
+                  href="https://github.com/chadiayari"
+                  style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                  href="https://www.facebook.com/chadi.ayari.1/"
+                  style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiFillFacebook />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.linkedin.com/in/chadi-ayari-7a11a6157/"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.instagram.com/ayari_chadi/"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiFillInstagram />
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Footer;
---
+++ src/components/Home/Type.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+//import Typewriter from "typewriter-effect";
+
+function Type() {
+  return (
+    // <Typewriter
+    //   options={{
+    //     strings: [
+    //       "Developer",
+    //       "Deep Learning Engineer",
+    //       "MERN Stack Developer",
+    //       "Open Source Contributor",
+    //     ],
+    //     autoStart: true,
+    //     loop: true,
+    //     deleteSpeed: 50,
+    //   }}
+    // />
+    <Container></Container>
+
+  );
+}
+
+export default Type;
